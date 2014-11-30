d06f7a49b9d2cdfc08968b8ea20080fe69ddd2db
mrgaolei <gaolei@liheran.com>
2014-11-30 21:54:10 +0800

    add header file

---
+++ Merry6SDK.h
@@ -0,0 +1,29 @@
+//
+//  Merry6SDK.h
+//  Merry6SDK
+//
+//  Created by mrgaolei on 14/11/10.
+//  Copyright (c) 2014 MerrySix. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@protocol Merry6SDKDelegate <NSObject>
+
+- (void)merry6SDKOrderCompleted;
+- (void)merry6SDKUserCancelled;
+
+@end
+
+@interface Merry6SDK : NSObject
+
+@property (nonatomic, strong) NSString *accessKey;
+
+@property (nonatomic, weak) id<Merry6SDKDelegate> delegate;
+
++ (instancetype)sharedM6SDK;
++ (NSString *)m6PathForResource:(NSString *)resource ofType:(NSString *)type;
+- (UIViewController *)newMerry6ViewController:(UIImage *)phoneCase;
+
+@end
